1541e5e97c5670e3d2456e4acdb85a03992d0d69
AtmatiAdi <32931612+AtmatiAdi@users.noreply.github.com>
2021-12-28 22:37:06 +0100

    points2Lines and lines2Poits functions

--- Lab_3/script.py
+++ Lab_3/script.py
@@ -1,5 +1,5 @@
 from time import sleep
-from drive import RosAriaDriver
+#from drive import RosAriaDriver
 import math
 from math import sin, cos
 import json
@@ -16,6 +16,43 @@ def pol2cart(rho, phi):
 	y = rho*np.sin(phi)
 	return(x,y)
 
+def lines2Points(lines):
+	X = []
+	Y = []
+	for l in range(len(lines)-1):
+		x1, y1, a1, b1, phi1 = lines[l]		# p1
+		x2, y2, a2, b2, phi2 = lines[l+1]	# p2
+		A = [[a1,-1],
+			 [a2,-1]]
+		C = [[x],
+			 [y]]
+		B =	[[-b1],
+			 [-b2]]
+		C = np.linalg.inv(A).dot(B)
+		X.append(C[0])
+		Y.append(C[1])
+	return [X,Y]
+		
+def points2Lines(X, Y, buf):
+	Lines = []
+	n = len(X)
+	for p in range(n-buf):
+		x = X[p:p+buf]
+		y = Y[p:p+buf]
+		print("X: " + str(x) + " Y: " + str(y))
+		model = np.polyfit(np.array(x), np.array(y) , 1)
+		#print(model)
+		#ffit = np.poly1d(model)
+		a = model[0]
+		b = model[1]
+		phi = np.rad2deg(np.arctan(a))
+		Lines.append([x,y,a,b,phi])
+	return Lines
+	
+###################################################################################
+# BEGIN
+###################################################################################
+
 x = np.arange(0,512)
 theta = (np.pi/512 )*x  # theta - scan angles in [rad]
 
@@ -24,6 +61,7 @@ ax1 = fig1.add_axes([0.1,0.1,0.8,0.8],polar=True)
 line, = ax1.plot(theta,data,'.',lw=1.5)
 ax1.set_ylim(0,5) 
 
+# Point to X and Y arrays calcullation
 points = []
 X = []
 Y = []
@@ -34,7 +72,9 @@ for p in range(512):
 		points.append([x,y])
 		X.append(x)
 		Y.append(y)
-		print(data[p])
+		#print(data[p])
+
+# Points to Lines calcullation
 n = len(points)	
 lines = []
 Phi = []
@@ -45,31 +85,61 @@ for p in range(n - 1):
 	lines.append([p1,p2,phi])
 	Phi.append(phi)
 	
-	
-pol_lines = []
-pol_Phi = []
-buf = 5
-for p in range(n-buf):
-	x = X[p:p+buf]
-	y = Y[p:p+buf]
-	p1 = points[p]
-	p2 = points[p+buf]
-	#print(x)
-	model = np.polyfit(np.array(x), np.array(y) , 1)
-	#ffit = np.poly1d(model)
-	a = 0.5
-	phi = np.arctan(a)
-	pol_lines.append([p1,p2,phi])
-	pol_Phi.append(phi)
+#######################################
+#######################################
+#######################################
+pol_lines = points2Lines(X, Y, 3)
+print("Ilosc lini surowych: " + str(len(pol_lines)))
+pol_X, pol_y = lines2Points(pol_lines)
+
+# Filtrate lines
+sens = 45
+new_lines = []
+while len(pol_lines) > 2:	# WAZNE GUBI OSTATNIA LINIE ??
+	x1, y1, a1, b1, phi1 = pol_lines.pop(0)		# p1
+	x2, y2, a2, b2, phi2 = pol_lines.pop(1) 	# p2
+	x = x2.copy()
+	y = y2.copy()
+	#print("x1: " + str(x1))
+	#print("x2: " + str(x2))
+	# Jesli roznica miedzy dwoma liniami jest ponizej sens to scal je
+	if math.fabs(phi1 - phi2) <= sens:
+		# transfer p1 do p2
+		while len(x1) > 0:
+			exist = False
+			for i in range(len(x2)):
+				if (x1[0] == x2[i]) and (y1[0] == y2[i]):
+					# w p2 jest punkt z p1, nie dodajemy go do p2
+					exist = True
+					#print("exists")
+					continue
+			if exist == False:
+				x.append(x1[0])
+				y.append(y1[0])
+			x1.pop(0)
+			y1.pop(0)
+		model = np.polyfit(np.array(x), np.array(y) , 1)
+		a = model[0]
+		b = model[1]
+		phi = np.rad2deg(np.arctan(a))
+		# Zapisanie scalonych lini
+		new_lines.append([x,y,a,b,phi])
+	else:
+		new_lines.append([x1,y1,a1,b1,phi1])
+		new_lines.append([x2,y2,a2,b2,phi2])
+	#print("x2: " + str(x))
+	#input("")
+print("Ilosc lini po scalaniu: " + str(len(new_lines)))
 
 fig2 = plt.figure()
 ax2 = fig2.gca()
 line, = ax2.plot(X,Y,'.')
+line, = ax2.plot(pol_X,pol_y)
 #plt.show()
 
 fig3 = plt.figure()
 ax3 = fig3.gca()
 line, = ax3.plot(Phi)
-line, = ax3.plot(pol_Phi)
+#line, = ax3.plot(pol_Phi)
 plt.show()
 	
