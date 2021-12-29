44bd8e0b75178a1423a7018aee9745efcb31acb7
AtmatiAdi <32931612+AtmatiAdi@users.noreply.github.com>
2021-12-29 20:26:34 +0100

    XD

--- Lab_3/script.py
+++ Lab_3/script.py
@@ -1,12 +1,8 @@
 from time import sleep
 #from drive import RosAriaDriver
 import math
-from math import sin, cos
+from math import fabs, sin, cos
 import json
-
-json_data = open('box.json')
-data = json.load(json_data)
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -24,8 +20,6 @@ def lines2Points(lines):
 		x2, y2, a2, b2, phi2 = lines[l+1]	# p2
 		A = [[a1,-1],
 			 [a2,-1]]
-		C = [[x],
-			 [y]]
 		B =	[[-b1],
 			 [-b2]]
 		C = np.linalg.inv(A).dot(B)
@@ -48,98 +42,82 @@ def points2Lines(X, Y, buf):
 		phi = np.rad2deg(np.arctan(a))
 		Lines.append([x,y,a,b,phi])
 	return Lines
-	
+
+def sonar2Points(data):
+	X = []
+	Y = []
+	for p in range(512):
+		phi = 360/512 * p/2
+		if (math.isinf(data[p]) != True) and (math.isnan(data[p]) != True):
+			x, y =pol2cart(data[p], np.deg2rad(phi))
+			#points.append([x,y])
+			X.append(x)
+			Y.append(y)
+			#print(data[p])
+	return X,Y
+
+def filterLines(lines, sens):
+	new_lines = []
+	while len(lines) > 2:	# WAZNE GUBI OSTATNIA LINIE ??
+		x1, y1, a1, b1, phi1 = lines.pop(0)		# p1
+		x2, y2, a2, b2, phi2 = lines.pop(1) 	# p2
+		x = x2.copy()
+		y = y2.copy()
+		#print("x1: " + str(x1))
+		#print("x2: " + str(x2))
+		# Jesli roznica miedzy dwoma liniami jest ponizej sens to scal je
+		if math.fabs(phi1 - phi2) <= sens:
+			# transfer p1 do p2
+			while len(x1) > 0:
+				exist = False
+				for i in range(len(x2)):
+					if (x1[0] == x2[i]) and (y1[0] == y2[i]):
+						# w p2 jest punkt z p1, nie dodajemy go do p2
+						exist = True
+						#print("exists")
+						continue
+				if exist == False:
+					x.append(x1[0])
+					y.append(y1[0])
+				x1.pop(0)
+				y1.pop(0)
+			model = np.polyfit(np.array(x), np.array(y) , 1)
+			a = model[0]
+			b = model[1]
+			phi = np.rad2deg(np.arctan(a))
+			# Zapisanie scalonych lini
+			new_lines.append([x,y,a,b,phi])
+		else:
+			new_lines.append([x1,y1,a1,b1,phi1])
+			new_lines.append([x2,y2,a2,b2,phi2])
+		#print("x2: " + str(x))
+		#input("")
+	return new_lines
 ###################################################################################
 # BEGIN
 ###################################################################################
-
+json_data = open('box.json')
+data = json.load(json_data)
 x = np.arange(0,512)
 theta = (np.pi/512 )*x  # theta - scan angles in [rad]
-
-fig1 = plt.figure()
-ax1 = fig1.add_axes([0.1,0.1,0.8,0.8],polar=True) 
-line, = ax1.plot(theta,data,'.',lw=1.5)
-ax1.set_ylim(0,5) 
-
-# Point to X and Y arrays calcullation
-points = []
-X = []
-Y = []
-for p in range(512):
-	phi = 360/512 * p/2
-	if (math.isinf(data[p]) != True) and (math.isnan(data[p]) != True):
-		x, y =pol2cart(data[p], np.deg2rad(phi))
-		points.append([x,y])
-		X.append(x)
-		Y.append(y)
-		#print(data[p])
-
-# Points to Lines calcullation
-n = len(points)	
-lines = []
-Phi = []
-for p in range(n - 1):
-	p1 = points[p]
-	p2 = points[p+1]
-	phi = np.rad2deg(np.arctan((p1[1]-p2[1])/(p1[0] - p2[0])))
-	lines.append([p1,p2,phi])
-	Phi.append(phi)
-	
-#######################################
-#######################################
-#######################################
-pol_lines = points2Lines(X, Y, 3)
-print("Ilosc lini surowych: " + str(len(pol_lines)))
-pol_X, pol_y = lines2Points(pol_lines)
-
-# Filtrate lines
-sens = 45
-new_lines = []
-while len(pol_lines) > 2:	# WAZNE GUBI OSTATNIA LINIE ??
-	x1, y1, a1, b1, phi1 = pol_lines.pop(0)		# p1
-	x2, y2, a2, b2, phi2 = pol_lines.pop(1) 	# p2
-	x = x2.copy()
-	y = y2.copy()
-	#print("x1: " + str(x1))
-	#print("x2: " + str(x2))
-	# Jesli roznica miedzy dwoma liniami jest ponizej sens to scal je
-	if math.fabs(phi1 - phi2) <= sens:
-		# transfer p1 do p2
-		while len(x1) > 0:
-			exist = False
-			for i in range(len(x2)):
-				if (x1[0] == x2[i]) and (y1[0] == y2[i]):
-					# w p2 jest punkt z p1, nie dodajemy go do p2
-					exist = True
-					#print("exists")
-					continue
-			if exist == False:
-				x.append(x1[0])
-				y.append(y1[0])
-			x1.pop(0)
-			y1.pop(0)
-		model = np.polyfit(np.array(x), np.array(y) , 1)
-		a = model[0]
-		b = model[1]
-		phi = np.rad2deg(np.arctan(a))
-		# Zapisanie scalonych lini
-		new_lines.append([x,y,a,b,phi])
-	else:
-		new_lines.append([x1,y1,a1,b1,phi1])
-		new_lines.append([x2,y2,a2,b2,phi2])
-	#print("x2: " + str(x))
-	#input("")
-print("Ilosc lini po scalaniu: " + str(len(new_lines)))
+X,Y = sonar2Points(data)
+lines = points2Lines(X, Y, 2)
+print("Ilosc lini surowych: " + str(len(lines)))
+pol_X, pol_Y = lines2Points(lines)
 
 fig2 = plt.figure()
 ax2 = fig2.gca()
 line, = ax2.plot(X,Y,'.')
-line, = ax2.plot(pol_X,pol_y)
-#plt.show()
+line, = ax2.plot(pol_X,pol_Y)
 
-fig3 = plt.figure()
-ax3 = fig3.gca()
-line, = ax3.plot(Phi)
-#line, = ax3.plot(pol_Phi)
+
+lines = filterLines(lines, 15)
+
+print("Ilosc lini po scalaniu: " + str(len(lines)))
+
+new_X, new_Y = lines2Points(lines)
+line, = ax2.plot(new_X,new_Y)
+
+
 plt.show()
 	
