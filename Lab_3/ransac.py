32c3254dd506e4bb706709a2c2ba8cf98089f163
AtmatiAdi <32931612+AtmatiAdi@users.noreply.github.com>
2022-01-25 20:31:21 +0100

    ransac update

--- Lab_3/ransac.py
+++ Lab_3/ransac.py
@@ -83,40 +83,84 @@ plt.xlim([-5, 5])
 plt.ylim([-5, 5])
 plt.savefig('tmp_map.png', dpi=200)
 
-iterations = 10000
+neighborhood_size = 10
+search_neighborhood_size = 60
+pts_low_treshold = 30
+#pts_treshold_step = 5
+#pts_min_treshold = 10
+pts_sense = 0.02
+pts_oversense = 0.01
 
-for j in range(1, iterations):
-    X_random = []
-    Y_random = []
-    for i in range(0, 15):
-        random_index = random.randint(0, len(X)-1)
-        X_random.append(X[random_index])
-        Y_random.append(Y[random_index])
+line_is_detected = 1
 
-    tmp_line = np.polyfit(np.array(X_random), np.array(Y_random), 1)
+while (line_is_detected):
+    best_fitted_pts_count = 0
+    line_is_detected = 0
+    best_random_first_index = 0
+    best_line = 0
+    for j in range(1, len(X)-1):
+        X_random = []
+        Y_random = []
+        random_first_index = j
+        neig_size_up = neig_size_down = neighborhood_size//2
+        if (random_first_index + neig_size_up) > len(X)-1: neig_size_up = (len(X)-1) - random_first_index
+        if (random_first_index - neig_size_down) < 0: neig_size_down = random_first_index
+        for i in range(random_first_index - neig_size_down, random_first_index + neig_size_up):
+            X_random.append(X[i])
+            Y_random.append(Y[i])
 
-    threshold = 0.02
-    fitted_pts_count = 0
+        tmp_line = np.polyfit(np.array(X_random), np.array(Y_random), 1)
+        fitted_pts_count = 0
 
-    for i in range(0, len(X)-1):
-        if dist_point_to_line(X[i], Y[i], tmp_line[0], -1, tmp_line[1]) < threshold:
-            fitted_pts_count = fitted_pts_count + 1
+        neig_size_up = neig_size_down = search_neighborhood_size//2
+        if (random_first_index + neig_size_up) > len(X)-1: neig_size_up = (len(X)-1) - random_first_index
+        if (random_first_index - neig_size_down) < 0: neig_size_down = random_first_index
+        for i in range(random_first_index - neig_size_down, random_first_index + neig_size_up):
+            if dist_point_to_line(X[i], Y[i], tmp_line[0], -1, tmp_line[1]) < pts_sense:
+                fitted_pts_count = fitted_pts_count + 1
 
-    if fitted_pts_count > 30:
-        x0 = -2.5
-        y0 = tmp_line[0] * x0 + tmp_line[1]
-        x1 = 1
-        y1 = tmp_line[0] * x1 + tmp_line[1]
-        plot_line = ax.plot([x0, x1], [y0, y1])
+        if (fitted_pts_count > pts_low_treshold) > best_fitted_pts_count:
+            best_fitted_pts_count = fitted_pts_count
+            best_line = tmp_line
+            line_is_detected = 1
+            best_random_first_index = random_first_index
 
-        pts_to_delete = []
-        for i in range(0, len(X) - 1):
-            if dist_point_to_line(X[i], Y[i], tmp_line[0], -1, tmp_line[1]) < threshold:
-                pts_to_delete.append(i)
+    if best_fitted_pts_count > pts_low_treshold:
 
-        for i in range(len(pts_to_delete) - 1, 0, -1):
-            X.pop(pts_to_delete[i])
-            Y.pop(pts_to_delete[i])
+        down_pts_to_delete = []
+        upper_pts_to_delete = []
+        # form best_random_first_index to end
+        for i in range(best_random_first_index, len(X) - 1):
+            # CHeck if poit belongs to the line
+            if (math.dist([X[i-1], Y[i-1]], [X[i], Y[i]]) < 10*(pts_sense + pts_oversense)): 
+                #print("point [" + str(X[i-1]) + ","+str(Y[i-1]) +"]" +"point [" + str(X[i]) + ","+str(Y[i]) +"] dist: " + str(math.dist([X[i-1], Y[i-1]], [X[i], Y[i]])))
+                if dist_point_to_line(X[i], Y[i], best_line[0], -1, best_line[1]) < pts_sense + pts_oversense:
+                    upper_pts_to_delete.append(i)
+                else: break
+        print("-----")
+        # from best_random_first_index to first
+        for i in range(best_random_first_index, 0, -1):
+            # CHeck if poit belongs to the line
+            if (math.dist([X[i+1], Y[i+1]], [X[i], Y[i]]) < 5*(pts_sense + pts_oversense)):
+                #print("point [" + str(X[i+1]) + ","+str(Y[i+1]) +"]" +"point [" + str(X[i]) + ","+str(Y[i]) +"] dist: " + str(math.dist([X[i+1], Y[i+1]], [X[i], Y[i]])))
+                if dist_point_to_line(X[i], Y[i], best_line[0], -1, best_line[1]) < pts_sense + pts_oversense:
+                    down_pts_to_delete.append(i)
+                else: break
+
+        plot_line = ax.plot([X[down_pts_to_delete[-1]], X[upper_pts_to_delete[-1]]], [Y[down_pts_to_delete[-1]], Y[upper_pts_to_delete[-1]]])
+
+        for i in range(len(upper_pts_to_delete) - 1, 0, -1):
+            X.pop(upper_pts_to_delete[i])
+            Y.pop(upper_pts_to_delete[i])
+
+        for i in range(0, len(down_pts_to_delete)):
+            X.pop(down_pts_to_delete[i])
+            Y.pop(down_pts_to_delete[i])
+    # if (line_is_detected == 0) and (pts_low_treshold > pts_min_treshold):
+    #     line_is_detected = 1
+    #     pts_low_treshold -= pts_treshold_step
+    #     pts_sense += pts_oversense/2
+    #     print(pts_low_treshold)
 
 
 plot_line = ax.plot(X, Y, 'r.')
